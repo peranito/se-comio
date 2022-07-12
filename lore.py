8f0f656753f46f9cfb353f8aa9941c0f28003a65
Andres252 <andres090454@gmail.com>
2022-07-12 15:25:15 -0500

    ola peranito

---
+++ lore.py
@@ -0,0 +1,26 @@
+print("**hola**")
+nombre = input("Como te llamas?_")
+print("holiii",nombre)
+input("que te gusta hacer?_")
+input("suena interesante :D_")
+edad=input("que edad tienes?_")
+print(nombre,"tienes",edad,"años")
+if edad >"18":
+    print("vamos de party")
+if  edad <"18":
+    print("veo la carcel en mi futuro")
+if edad=="18":
+    print("tienes cordon")
+gustos=input("te gusta la plata $$?_")
+if gustos =="si":
+    print ("me compras un aifon")
+if gustos =="no":
+    print("por que no?")
+if gustos =="tal vez":
+    print("COMO QUE TALVEZ PERRO HP ESA NO ES UNA RESPUESTA")
+amor=input("te gusta alguien estas enamorado ?_")
+
+    
+
+
+
