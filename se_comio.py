4027086a5a55d51cd42ef36aec8c4a607fefd1c8
Andres252 <andres090454@gmail.com>
2022-07-12 15:52:45 -0500

    tengo sueño

---
+++ se_comio.py
@@ -0,0 +1,36 @@
+usuario1="@pepito_pereZ"
+intentos="2"
+usuario2="peranito@nesecitoplatasoypobre"
+password1="gato1234"
+
+print ("hola")
+print ("deseas seguir como @pepito_pereZ?")
+seguir=input()
+if seguir == "si":
+    print ("porfavor inserte la contraseña")
+    password2=input()
+    if password1==password2:
+        print("puedes pasar puedes entrar")
+    
+    else:
+        for i in range (0):
+            if password1==password2:
+                print ("puedes entrer")
+                break
+            else:
+                print("intentalo de nuevo no creo que sea eso")
+                gato1234=input()
+    
+if seguir== "no":
+    print ("inserta tu usuario")
+    usuario2= input()
+    
+else:
+    for i in range (2):
+        if password1==password2:
+            print ("puedes entrer")
+            break
+        else:
+            print("intentalo de nuevo no creo que sea eso")
+            gato1234=input()
+    
